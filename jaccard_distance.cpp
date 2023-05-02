d03b2f1333273c5033c72d420892d7364c2e48ad
IImbryk <3533868@gmail.com>
2023-05-02 07:46:34 +0300

    Create jaccard_distance.cpp

---
+++ jaccard_distance.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <chrono>
+using namespace std::chrono;
+
+
+using namespace std;
+ 
+double array_similarity(int arr1[], int arr2[], int n) {
+
+    // sort(arr1, arr1 + n);
+    // sort(arr2, arr2 + n);
+    
+    set <int> set1(arr1, arr1 + n);
+    set <int> set2(arr2, arr2 + n);
+    int intersection_size = 0;
+    for (int x : set1) {
+        if (set2.count(x)) {
+            intersection_size++;
+        }
+    }
+    int union_size = set1.size() + set2.size() - intersection_size;
+    return (double)intersection_size / union_size;
+}
+ 
+int main() {
+    int arr1[] = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+    int arr2[] = {4, 5, 8, 6, 10, 4, 5, 8, 6, 10};
+    int n = 10;
+    
+    auto start = high_resolution_clock::now();
+    double similarity = array_similarity(arr1, arr2, n);
+    auto stop = high_resolution_clock::now();
+    cout << "The similarity coefficient is " << similarity << endl;
+    
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "1 Time taken by function: "
+         << duration.count() << " microseconds" << endl;
+
+    return 0;
+}
